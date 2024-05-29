b1e3750778080cbad5f8bffb7a8fbecb741b9951
IvanMagda <ivanmagda164@gmail.com>
2024-05-29 12:37:17 +0300

    Initial Commit

---
+++ TrafficFactoryTestTask/TrafficFactoryTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficFactoryTestTaskApp.swift
+//  TrafficFactoryTestTask
+//
+//  Created by Ivan Magda on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficFactoryTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
